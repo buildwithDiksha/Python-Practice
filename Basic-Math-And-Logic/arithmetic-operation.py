cac3c17d6792a34656a32cae227993d968825c6b
Diksha Sobhawat <dikshasobhawat231@gmail.com>
2026-06-29 18:00:27 +0530

    basic arithmetic operation question

---
+++ Basic-Math-And-Logic/arithmetic-operation.py
@@ -0,0 +1,11 @@
+a = int(input("Enter first number :"))
+b = int(input("Enter second number :"))
+sum = a + b ;
+difference = a -b ;
+product = a * b ;
+quotient = a / b ;
+
+print("Sum of a and b is : " , sum )
+print("Difference of a and b is : " , difference)
+print("Product of a and b is : " , product)
+print("Quotient of a and b is : " , quotient)
