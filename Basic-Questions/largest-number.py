efe14f075b77fb32f31d4bebb9166e47e78f50c4
Diksha Sobhawat <dikshasobhawat231@gmail.com>
2026-06-29 19:22:32 +0530

    largest number checker

---
+++ Basic-Questions/largest-number.py
@@ -0,0 +1,11 @@
+a = int(input("Enter first number : "))
+b = int(input("Enter second number : "))
+c = int(input("Enter third number : "))
+
+if (a>b and c<a):
+    print(a ,"is maximum number ")
+elif(b>a and c<b):
+    print(b ," is maximum number ")    
+else:
+    print(c,"is maximum number")    
+
