a9cdd914cef934b05c749f087fa552aa9c83ff79
Diksha Sobhawat <dikshasobhawat231@gmail.com>
2026-07-02 18:00:11 +0530

    0-1 pattern print

---
+++ Loop-Questions/0-1-pattern.py
@@ -0,0 +1,8 @@
+N = 5 
+for i in range(1,6):
+    for j in range(i):
+        print(j % 2 ,end = " ")
+    print()    
+    
+
+# yaha pr jb i=1 hoga to j=0 hoga ,fir i=2 to j=0,1  , fir i=3 to j=0,1,2 to yaha pr 2 aagya to isliye j ko 2 se divide kiya jo remainder dega aur 0 print ho jaega 
