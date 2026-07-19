0240d93d79c20f54cf30f02b981a2c9976846c3a
Diksha Sobhawat <dikshasobhawat231@gmail.com>
2026-07-19 15:20:56 +0530

    merged two arrays into third array

---
+++ Lists/merge-array.py
@@ -0,0 +1,11 @@
+arr1 = [10,20,30]
+arr2 = [40,50,60]
+merged =  []
+for i in range(len(arr1)):
+    merged.append(arr1[i])
+
+for j in range(len(arr2)):
+    merged.append(arr2[j])
+
+print(merged)        
+    
